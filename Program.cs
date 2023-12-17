45969127e0d3a097681e340d53a1a6f575ab03c6
星辰德鲁伊 <xingchendelu1@outlook.com>
2023-12-17 12:22:56 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace xingchendelu1_20231217_v10_gemini_curve_II
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
